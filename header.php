5343a1e9e6e927ac2c51d223dac1a7e978f27d22
Joel Krause <35904580+joelkrause@users.noreply.github.com>
2018-11-05 21:41:41 +1100

    build 1.0.9
    added social links to header

--- header.php
+++ header.php
@@ -28,7 +28,11 @@
                     ?>
                     </nav>
                     <div class="social--links">
-                        <a href="https://twitter.com/Joel_Krause">Twitter</a>
+                        <ul>
+                            <li><a href="https://twitter.com/Joel_Krause" target="_blank"><img src="<?php echo get_template_directory_uri();?>/lib/images/twitter.svg" /></a></li>
+                            <li><a href="https://www.linkedin.com/in/joel-krause/" target="_blank"><img src="<?php echo get_template_directory_uri();?>/lib/images/linkedin.svg" /></a></li>
+                            <li><a href="https://github.com/joelkrause" target="_blank"><img src="<?php echo get_template_directory_uri();?>/lib/images/github.svg" /></a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
